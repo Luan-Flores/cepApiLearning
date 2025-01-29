3ae45080b6699b4d14506936d88f63e18e878534
luan <keeganluan919@gmail.com>
2025-01-29 10:25:12 -0400

    Consulta ao CEP pela API finalizada

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const cep = document.querySelector("#cep");
+const cep = document.querySelector("#cep"); //Buscando o input com id CEP
 
+//Método para mostrar os dados que retornarem (result) da API 
 const showData = (result)=>{
     for(const campo in result){
+        //Retornando apenas os campos do result que equivalem ao ID dos inputs do form HTML
         let condition = document.querySelector(`#${campo}`);
         if(condition){
             console.log(campo);
@@ -11,31 +13,38 @@ const showData = (result)=>{
         }
     }
 }
-
-
-
-
-
-
-
-
+//Adicionando um evento com o blur (quando o usuário digitar o CEP e clicar em outra parte da tela)
 cep.addEventListener("blur",(e)=>{
-    let search = cep.value.replace("-","");
+    let search = cep.value.replace("-",""); //Substituindo o "-" do CEP para que a API seja consultada somente com os números
+    
+    //Declarando os padrões de consulta
     const options = {
-        method: 'GET',
-        mode: 'cors',
-        cache: 'default'
+        method: 'GET',     //Buscar dados do servidor API (padrão do fetch)
+        mode: 'cors',     //Permite que a requisição seja feita para outro domínio se o servidor permitir
+        cache: 'default' //Usa o comportamento padrão do navegador
     }
+
+    //Se o campo do CEP estiver vazio, voltar aos valores default;
     if(cep.value === ""){
         console.log("vazio");
+    
+        // Limpa todos os inputs do formulário
+        document.querySelectorAll("input").forEach(input => {
+            if (input.id !== "cep") { // Mantém o campo CEP sem alteração
+                input.value = "";
+            }
+        });
+    
         return;
     }
-
-    fetch(`https://viacep.com.br/ws/${search}/json/`, options)
-    .then(response=>{response.json()
-        .then( data => showData(data))
-        })
-    .catch(e => console.log('Deu erro: ' + e))
     
+    //Fetch na API para buscar os dados de acordo com o CEP que o usuário digitou
+    fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+
+    .then(response=>{response.json()    //Se a consulta der certo, trazer a response em formato JSON
+        .then( data => showData(data)) //Se trazer em JSON, retorne os dados com a função (showData)
+        })
+        
+    .catch(e => console.log('Deu erro: ' + e))
 
 })
